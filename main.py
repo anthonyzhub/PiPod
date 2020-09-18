7c6eb830285a4a913c75130243ab8140b2db26c0
Anthony Zamora <zamora.anthony1234@gmail.com>
2020-09-18 17:12:17 -0400

    Added 'Music' button

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #! /usr/bin/python3
 
 # Import libraries
-import tkinter
+from tkinter import *
+
+# Global variables
+WIN_WIDTH = 240
+WIN_HEIGHT = 240
+
+def music_window():
+
+	# OBJECTIVE: Open a new window with a list of songs
+	print("Music window is open")
 
 if __name__ == "__main__":
-	print("Hello, world!")
+
+	# Initialize tkinter()
+	root = Tk()
+
+	# Create a 240x240 window (dimensions comes from adafruit screen)
+	root.geometry("{}x{}".format(WIN_WIDTH, WIN_HEIGHT))
+
+	# Create a "Music" button to show user's music
+	music_btn = Button(root, text="Music", command=music_window)
+	music_btn.place(x=0, y=0, height=25, width=WIN_WIDTH)
+
+	# Display window with all widgets
+	root.mainloop()
